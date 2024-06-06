4c14c6b0abc6ebf7b982780c1b08b7598ac34c50
SaralThapaliya <saralthapaliya11@gmail.com>
2024-06-06 18:34:26 +0545

    sdf

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def main():
     number=st.slider("Pick a number")
-    st.text('The square number is {number**2}'.format(number))
+    st.text(f'The square number is {number**2}')
 
 
 
